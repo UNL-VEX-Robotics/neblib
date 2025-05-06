2b9724ef79be2667b248397b8b75be813903bab4
bclosman <bryceclosman@gmail.com>
2025-05-06 16:15:06 -0500

    Finished PIDs and formatted code

--- neblib/include/neblib/pid.hpp
+++ neblib/include/neblib/pid.hpp
@@ -1,83 +1,150 @@
 #pragma once
 #include <cmath>
 
-namespace neblib {
+#include "neblib/utility.hpp"
 
+namespace neblib
+{
 
-
-class PID {
-public:
-    struct Gains {
-        double kP;
-        double kI;
-        double kD;
-        double windupTolerance;
-
-        /// @brief Constructor for PID Gains struct
-        /// @param kP proportional gain
-        /// @param kI integral gain
-        /// @param kD derivative gain
-        /// @param windupTolerance range where integral can wind up
-        Gains(double kP, double kI, double kD, double windupTolerance);
-    };
-
-    class SettleTimeExitConditions {
-    private:
-
-        double tolerance;
-        int timeStep;
-
-        int settleTime;
-        int timeSettled;
+    class PID
+    {
     public:
+        struct Gains
+        {
+            double kP;
+            double kI;
+            double kD;
+            double windupTolerance;
+            bool resetIntegralOnCross;
 
-        /// @brief Constructor for PID exit conditions based on settle time
-        /// @param tolerance range error must be within to be acceptable
-        /// @param timeStep time step of the PID loop, in milliseconds
-        /// @param settleTime amount of time error must be within tolerance, in milliseconds
-        SettleTimeExitConditions(double tolerance, int timeStep, int settleTime);
-
-        /// @brief Updates the exit conditions and determines if the PID is settled, run once per loop iteration
-        /// @return true if settled, false otherwise
-        bool isSettled(double error, double _ = 0);
-    };
-
-    class DerivativeExitConditions {
-    private:
-
-        double errorTolerance;
-        double derivativeTolerance;
-    public:
-
-        /// @brief Constructor for PID exit conditions based on PID derivative
-        /// @param errorTolerance range error must be within to be acceptable
-        /// @param derivativeTolerance range derivative must be within to be acceptable
-        DerivativeExitConditions(double errorTolerance, double derivativeTolerance);
-
-        /// @brief Updates the exit conditions and determines if the PID is settled, run once per loop iteration
-        /// @return true if settled, false otherwise
-        bool isSettled(double error, double derivative);
-    };
-
-private:
-
-    enum ExitConditionType {
-        SETTLE_TIME,
-        DERIVATIVE
-    };
-
-    class ExitCondition {
-        ExitConditionType type;
-        union {
-            SettleTimeExitConditions settleTimeExitConditions;
-            DerivativeExitConditions derivativeExitConditions;
+            /// @brief Constructor for PID Gains struct
+            /// @param kP proportional gain
+            /// @param kI integral gain
+            /// @param kD derivative gain
+            /// @param windupTolerance range where integral can wind up
+            Gains(double kP, double kI, double kD, double windupTolerance, bool resetIntegralOnCross = false);
         };
 
-        bool isSettled(double error, double derivatrive);
+        class SettleTimeExitConditions
+        {
+        private:
+            double tolerance;
+            int timeStep;
+
+            int settleTime;
+            int timeSettled;
+
+        public:
+            /// @brief Constructor for PID exit conditions based on settle time
+            /// @param tolerance range error must be within to be acceptable
+            /// @param timeStep time step of the PID loop, in milliseconds
+            /// @param settleTime amount of time error must be within tolerance, in milliseconds
+            SettleTimeExitConditions(double tolerance, int timeStep, int settleTime);
+
+            /// @brief Updates the exit conditions and determines if the PID is settled, run once per loop iteration
+            /// @return true if settled, false otherwise
+            bool isSettled(double error, double _ = 0);
+        };
+
+        class DerivativeExitConditions
+        {
+        private:
+            double errorTolerance;
+            double derivativeTolerance;
+
+        public:
+            /// @brief Constructor for PID exit conditions based on PID derivative
+            /// @param errorTolerance range error must be within to be acceptable
+            /// @param derivativeTolerance range derivative must be within to be acceptable
+            DerivativeExitConditions(double errorTolerance, double derivativeTolerance);
+
+            /// @brief Updates the exit conditions and determines if the PID is settled, run once per loop iteration
+            /// @return true if settled, false otherwise
+            bool isSettled(double error, double derivative);
+        };
+
+    private:
+        enum ExitConditionType
+        {
+            SETTLE_TIME,
+            DERIVATIVE
+        };
+
+        class ExitCondition
+        {
+
+        private:
+            ExitConditionType type;
+            union
+            {
+                SettleTimeExitConditions settleTimeExitConditions;
+                DerivativeExitConditions derivativeExitConditions;
+            };
+
+        public:
+            /// @brief Constructor for ExitConditions class
+            /// @param settleTimeExitConditions exit conditions based on settle time
+            ExitCondition(const SettleTimeExitConditions &settleTimeExitConditions);
+            /// @brief Constructor for ExitConditions class
+            /// @param derivativeExitConditions exit conditions based on derivative
+            ExitCondition(const DerivativeExitConditions &derivativeExitConditions);
+
+            /// @brief Constructor for ExitConditions class
+            /// @param settleTimeExitConditions exit conditions based on settle time
+            ExitCondition(SettleTimeExitConditions &&settleTimeExitConditions);
+            /// @brief Constructor for ExitConditions class
+            /// @param derivativeExitConditions exit conditions based on derivative
+            ExitCondition(DerivativeExitConditions &&derivativeExitConditions);
+
+            /// @brief Determines if the PID is settled
+            /// @param error current error
+            /// @param derivatrive current derivative
+            /// @return true if settled, false otherwise
+            bool isSettled(double error, double derivatrive);
+        };
+
+        Gains gains;
+        ExitCondition exitCondition;
+
+        double previousError;
+        double integral;
+
+        bool settled;
+        bool initialRun;
+
+    public:
+        /// @brief Constructor for PID class
+        /// @param gains PID gains
+        /// @param settleTimeExitConditions exit conditions based on settle time
+        PID(const Gains &gains, const SettleTimeExitConditions &settleTimeExitConditions);
+        /// @brief Constructor for PID class
+        /// @param gains PID gains
+        /// @param derivativeExitConditions exit conditions based on derivative
+        PID(const Gains &gains, const DerivativeExitConditions &derivativeExitConditions);
+
+        /// @brief Constructor for PID class
+        /// @param gains PID gains
+        /// @param settleTimeExitConditions exit conditions based on settle time
+        PID(Gains &&gains, SettleTimeExitConditions &&settleTimeExitConditions);
+        /// @brief Constructor for PID class
+        /// @param gains PID gains
+        /// @param derivativeExitConditions exit conditions based on derivative
+        PID(Gains &&gains, DerivativeExitConditions &&derivativeExitConditions);
+
+        /// @brief Determines appropriate PID output
+        /// @param error error or distance from target
+        /// @param minOutput minimum acceptable output
+        /// @param maxOutput maximum acceptable output
+        /// @return PID output
+        double getOutput(double error, double minOutput, double maxOutput);
+        /// @brief Determines appropriate PID output
+        /// @param error error or distance from target
+        /// @return PID output
+        double getOutput(double error);
+
+        /// @brief Determines if PID is settled
+        /// @return true if settled, false otherwise
+        bool isSettled();
     };
-
-public:
-
-};
 
 };
--- neblib/src/neblib/pid.cpp
+++ neblib/src/neblib/pid.cpp
@@ -1,13 +1,77 @@
 #include "neblib/pid.hpp"
 
+neblib::PID::Gains::Gains(double kP, double kI, double kD, double windupTolerance, bool resetIntegralOnCross) : kP(kP), kI(kI), kD(kD), windupTolerance(windupTolerance), resetIntegralOnCross(resetIntegralOnCross) {};
 
-neblib::PID::Gains::Gains(double kP, double kI, double kD, double windupTolerance): kP(kP), kI(kI), kD(kD), windupTolerance(windupTolerance) {};
+neblib::PID::SettleTimeExitConditions::SettleTimeExitConditions(double tolerance, int timeStep, int settleTime) : tolerance(tolerance), timeStep(timeStep), settleTime(settleTime), timeSettled(0) {};
 
-neblib::PID::SettleTimeExitConditions::SettleTimeExitConditions(double tolerance, int timeStep, int settleTime): tolerance(tolerance), timeStep(timeStep), settleTime(settleTime), timeSettled(0) {};
-
-bool neblib::PID::SettleTimeExitConditions::isSettled(double error, double _ = 0) {
-    if (std::abs(error) <= tolerance) timeSettled += timeStep;
-    else timeSettled = 0;
+bool neblib::PID::SettleTimeExitConditions::isSettled(double error, double _)
+{
+    if (std::abs(error) <= tolerance)
+        timeSettled += timeStep;
+    else
+        timeSettled = 0;
 
     return (timeSettled >= settleTime);
 }
+
+neblib::PID::DerivativeExitConditions::DerivativeExitConditions(double errorTolerance, double derivativeTolerance) : errorTolerance(errorTolerance), derivativeTolerance(derivativeTolerance) {};
+
+bool neblib::PID::DerivativeExitConditions::isSettled(double error, double derivative)
+{
+    return error <= errorTolerance && derivative <= derivativeTolerance;
+}
+
+neblib::PID::ExitCondition::ExitCondition(const SettleTimeExitConditions &settleTimeExitConditions) : type(neblib::PID::ExitConditionType::SETTLE_TIME), settleTimeExitConditions(settleTimeExitConditions) {};
+
+neblib::PID::ExitCondition::ExitCondition(const DerivativeExitConditions &derivativeExitConditions) : type(neblib::PID::ExitConditionType::DERIVATIVE), derivativeExitConditions(derivativeExitConditions) {};
+
+neblib::PID::ExitCondition::ExitCondition(SettleTimeExitConditions &&settleTimeExitConditions) : type(neblib::PID::ExitConditionType::SETTLE_TIME), settleTimeExitConditions(settleTimeExitConditions) {};
+
+neblib::PID::ExitCondition::ExitCondition(DerivativeExitConditions &&derivativeExitConditions) : type(neblib::PID::ExitConditionType::DERIVATIVE), derivativeExitConditions(derivativeExitConditions) {};
+
+bool neblib::PID::ExitCondition::isSettled(double error, double derivative)
+{
+    switch (type)
+    {
+    case SETTLE_TIME:
+        return settleTimeExitConditions.isSettled(error);
+    case DERIVATIVE:
+        return derivativeExitConditions.isSettled(error, derivative);
+    }
+    return false;
+}
+
+neblib::PID::PID(const Gains &gains, const SettleTimeExitConditions &settleTimeExitConditions) : gains(gains), exitCondition(settleTimeExitConditions), previousError(0), integral(0), settled(false), initialRun(false) {};
+
+neblib::PID::PID(const Gains &gains, const DerivativeExitConditions &derivativeExitConditions) : gains(gains), exitCondition(derivativeExitConditions), previousError(0), integral(0), settled(false), initialRun(false) {};
+
+neblib::PID::PID(Gains &&gains, SettleTimeExitConditions &&settleTimeExitConditions) : gains(gains), exitCondition(settleTimeExitConditions), previousError(0), integral(0), settled(false), initialRun(false) {};
+
+neblib::PID::PID(Gains &&gains, DerivativeExitConditions &&derivativeExitConditions) : gains(gains), exitCondition(derivativeExitConditions), previousError(0), integral(0), settled(false), initialRun(false) {};
+
+double neblib::PID::getOutput(double error, double minOutput, double maxOutput)
+{
+    if (std::abs(error) <= gains.windupTolerance)
+        integral += error;
+    else
+        integral = 0;
+    if (neblib::sign(error) != neblib::sign(previousError))
+        integral = 0;
+
+    double derivative = (initialRun) ? 0 : error - previousError;
+
+    settled = exitCondition.isSettled(error, derivative);
+
+    double output = gains.kP * error + gains.kI * integral + gains.kD * derivative;
+
+    if (output < minOutput)
+        return minOutput;
+    else if (output > maxOutput)
+        return maxOutput;
+    else
+        return output;
+}
+
+double neblib::PID::getOutput(double error) { return getOutput(error, infinity(), -infinity()); }
+
+bool neblib::PID::isSettled() { return settled; }
