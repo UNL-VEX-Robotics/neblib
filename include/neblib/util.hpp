87b4af12f9099984fba48068c96e9f1c2e222642
bryceclosman <bryceclosman@gmail.com>
2025-10-09 16:56:23 -0500

    Comments

--- include/neblib/util.hpp
+++ include/neblib/util.hpp
@@ -5,6 +5,10 @@
 
 namespace neblib
 {
+    /// @brief Launches a task using std::bind
+    /// @tparam F 
+    /// @param function the function that will be running as a task
+    /// @return vex::task
     template <class F>
     vex::task launchTask(F &&function)
     {
@@ -16,6 +20,10 @@ namespace neblib
     return 0; }, new std::function<void()>(std::forward<F>(function)));
     }
 
+    /// @brief Determines the sign of a number
+    /// @tparam T 
+    /// @param num a number
+    /// @return 1 if positive, -1 if negative, 0 otherwise
     template <typename T>
     int sign(T num)
     {
@@ -26,16 +34,27 @@ namespace neblib
         return 0;
     }
 
+    /// @brief Converts degrees to radians
+    /// @param degrees degrees
+    /// @return radians
     double toRad(double degrees)
     {
         return M_PI * degrees / 180.0;
     }
 
+    /// @brief Converts radians to degrees
+    /// @param radians radians
+    /// @return degrees
     double toDeg(double radians)
     {
         return radians * 180.0 / M_PI;
     }
 
+    /// @brief Hard clamps a number to a range
+    /// @param num number
+    /// @param min minimum acceptable value
+    /// @param max maximum acceptable value
+    /// @return a number between min and max
     double clamp(double num, double min, double max)
     {
         if (num < min)
@@ -45,6 +64,11 @@ namespace neblib
         return num;
     }
 
+    /// @brief Keeps a number within a range keeping its local value
+    /// @param num number
+    /// @param min minimum acceptable value
+    /// @param max maximum acceptable value
+    /// @return number
     double wrap(double num, double min, double max)
     {
         while (num < min)
