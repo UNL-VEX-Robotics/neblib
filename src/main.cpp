b399c57bc793a794c49e45ed8fb8160ab9a90d6d
bryceclosman <bryceclosman@gmail.com>
2025-10-09 20:57:18 -0500

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 /*----------------------------------------------------------------------------*/
 
 #include "vex.h"
-#include "neblib/xdrive.hpp"
+#include "neblib/odometry.hpp"
 #include <iostream>
 
 using namespace vex;
@@ -20,29 +20,14 @@ competition Competition;
 brain Brain;
 controller controller1(primary);
 
-motor leftFrontTop(PORT12, ratio6_1, false);
-motor leftFrontBottom(PORT13, ratio6_1, true);
-vex::motor_group leftFront(leftFrontTop, leftFrontBottom);
-motor rightFrontTop(PORT3, ratio6_1, true);
-motor rightFrontBottom(PORT2, ratio6_1, false);
-vex::motor_group rightFront(rightFrontTop, rightFrontBottom);
-motor leftBackTop(PORT14, ratio6_1, false);
-motor leftBackBottom(PORT15, ratio6_1, true);
-vex::motor_group leftBack(leftBackTop, leftBackBottom);
-motor rightBackTop(PORT5, ratio6_1, true);
-motor rightBackBottom(PORT4, ratio6_1, false);
-vex::motor_group rightBack(rightBackTop, rightBackBottom);
+vex::rotation r1 = vex::rotation(PORT10);
+vex::rotation r2 = vex::rotation(PORT11);
+vex::inertial imu(PORT1);
 
-inertial imu(PORT16);
+neblib::RotationTrackerWheel parallel = neblib::RotationTrackerWheel(r1, 2.0); 
+neblib::RotationTrackerWheel perpendicular = neblib::RotationTrackerWheel(r2, 2.0);
 
-neblib::TrackerWheel parallel(rotation(PORT11), 2.0);
-neblib::TrackerWheel perpendicular(rotation(PORT1), 2.0);
-neblib::Odometry odometry(&parallel, -3.5, &perpendicular, -1.1, &imu);
-
-neblib::XDrive xDrive(leftFront, rightFront, leftBack, rightBack, &odometry, &imu);
-
-neblib::PID rotationalPID(neblib::PID::Gains(0.135, 0.02, 0.25, 10.0), neblib::PID::SettleTimeExitConditions(0.25, 50, 10));
-neblib::PID linearPID(neblib::PID::Gains(0.3, 0.001, 0.5, 12.0, true), neblib::PID::SettleTimeExitConditions(0.25, 30, 10));
+neblib::Odometry odom = neblib::Odometry(parallel, 1.0, perpendicular, 2.0, imu);
 
 /*---------------------------------------------------------------------------*/
 /*                          Pre-Autonomous Functions                         */
@@ -58,15 +43,6 @@ void pre_auton(void) {
 
   // All activities that occur before the competition starts
   // Example: clearing encoders, setting servo positions, ...
-}
-
-int task_a_odometry()
-{
-  while(true)
-  {
-    odometry.updatePose();
-    task::sleep(10);
-  }
 }
 
 void autonomous(void) {
@@ -85,55 +61,12 @@ void autonomous(void) {
 /*  You must modify the code to add your own robot specific commands here.   */
 /*---------------------------------------------------------------------------*/
 
-int task_u_controllerPrint()
-{
-  while (true)
-  {
-    controller1.Screen.clearScreen();
-
-    neblib::Pose pose = odometry.getPose();
-    controller1.Screen.setCursor(1, 1);
-    controller1.Screen.print("Position: ");
-    controller1.Screen.setCursor(2, 1);
-    controller1.Screen.print("(");
-    controller1.Screen.print(pose.x);
-    controller1.Screen.print(", ");
-    controller1.Screen.print(pose.y);
-    controller1.Screen.print(", ");
-    controller1.Screen.print(pose.heading);
-    controller1.Screen.print(")");
-
-    controller1.Screen.setCursor(3, 1);
-    controller1.Screen.print(parallel.getPosition() / neblib::toRad(imu.rotation() - 90));
-
-    task::sleep(10);
-  }
-}
 
 void usercontrol(void) {
-  xDrive.setRotationalPID(&rotationalPID);
-  xDrive.setLinearPID(&linearPID);
-  Brain.Screen.clearScreen();
-  Brain.Screen.setCursor(1, 1);
-  Brain.Screen.print("Calibrating... ");
-  
-  
-  odometry.calibrate(5);
-  odometry.setPose(0, 0, 90);
-  
-  task a = task(task_a_odometry);
-  
-  // xDrive.driveToPose(24, 24, 90, 2);
-  // xDrive.driveToPose(24, 72, 90, 2);
-  // xDrive.driveToPose(0, 96, 90, 2);
-  // xDrive.driveToPose(-24, 72, 90, 2);
-  // xDrive.driveToPose(-24, 24, 90, 2);
-  // xDrive.driveToPose(0, 0, 90, 2);
-  
-
+ 
   while (true)
   {
-    xDrive.driveGlobal(controller1.Axis3.position(percent) * 0.12, controller1.Axis4.position(percent) * 0.12, controller1.Axis1.position(percent) * 0.12, vex::voltageUnits::volt);
+    
     task::sleep(10);
   }
 }
