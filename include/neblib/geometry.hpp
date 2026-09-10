4f8a44b264fa2d0d6610c4ad4312f953bb289601
bclosman <165829179+bclosman@users.noreply.github.com>
2026-09-10 12:20:08 -0500

    Created geometry file to store geometrical concepts like Point

---
+++ include/neblib/geometry.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace neblib
+{
+    struct Point
+    {
+        double x;
+        double y;
+        Point(double x, double y);
+    };
+}
---
+++ src/neblib/geometry.cpp
@@ -0,0 +1,6 @@
+#include "neblib/geometry.hpp"
+
+neblib::Point::Point(double x, double y):
+    x(x),
+    y(x)
+{}
